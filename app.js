9fead3fc99f5711c77dcfd594b67987986e8005a
Russell <russelldev@protonmail.com>
2020-12-27 17:25:26 +0000

    Fish out img from API

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const log = i => console.log(i);
 
 const form = document.querySelector('#searchForm');
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', async function(e){
     e.preventDefault();
-
-})
+    const searchTerm = form.elements.query.value;
+    const res = await axios.get(` http://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    log(res.data[0].show.image.medium);
+});
