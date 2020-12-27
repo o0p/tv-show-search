eb91eee69e4166d835c0747004290daf2e295d66
Russell <russelldev@protonmail.com>
2020-12-27 17:41:18 +0000

    Append found imgs to DOM

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const log = i => console.log(i);
+const log = (i) => console.log(i)
 
+const form = document.querySelector('#searchForm')
 
-const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function (e) {
+  e.preventDefault()
+  const searchTerm = form.elements.query.value
+  const res = await axios.get(
+    ` http://api.tvmaze.com/search/shows?q=${searchTerm}`
+  )
+  makeImages(res.data)
+  form.elements.query.value = '';
+})
 
-form.addEventListener('submit', async function(e){
-    e.preventDefault();
-    const searchTerm = form.elements.query.value;
-    const res = await axios.get(` http://api.tvmaze.com/search/shows?q=${searchTerm}`);
-    log(res.data[0].show.image.medium);
-});
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement('IMG')
+      img.src = result.show.image.medium
+      document.body.append(img)
+    }
+  }
+}
